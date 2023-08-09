55ef338cde33818ee271ee9e6d9002995fa1b533
LouisCR04 <louiskariuki04@gmail.com>
2023-08-09 18:27:22 +0300

    Added error msgs

--- main.cpp
+++ main.cpp
@@ -1,40 +1,89 @@
 #include <windows.h>
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
+
+void readprocess_errorMsg(void);
 
 int main(int argc, char **argv)
 {
     DWORD gold_value = 0;
-    DWORD new_gold_amount = 10000000;
+    DWORD new_gold_value = 10000;
     DWORD bytes_read = 0;
     DWORD process_id = 0;
     DWORD bytes_written = 0;
+    DWORD gld_val = 0;
 
     //if lpClassName is Null, it finds title that matches the lpWindowName param
     HWND window_handle = FindWindowA(NULL, "The Battle for Wesnoth - 1.14.9");
+    if (window_handle == NULL)
+    {
+        cout<< "Window Handle not found!\n";
+        system("pause");
+        exit(99);
+    }
+    cout << "Window Handle Successful: " << window_handle;
 
     //window_handle is obtained from the HWND FindWindowA
-    GetWindowThreadProcessId(window_handle, &process_id);
+    int _ThreadID = GetWindowThreadProcessId(window_handle, &process_id);
+    if (_ThreadID == 0)
+    {
+        cout<< "Thread Process Id not found!\n";
+        system("pause");
+        exit(99);
+    }
+    cout << "Thread Process Id Successful: " << _ThreadID;
 
     //targetProcessId is obtained from the GetWindowThreadProcessId API
     HANDLE wesnoth_process = OpenProcess(PROCESS_ALL_ACCESS, TRUE, process_id);
+    if (wesnoth_process == NULL)
+    {
+        cout << "Wesnoth_process not found!\n";
+        system("pause");
+        exit(99);
+    }
+    cout << "\nWesnoth_process Successful: " << wesnoth_process;
 
     //wesnoth_process is obtained from the OpenProcess API
     //base pointer is 0x017EECB8, we add 0x60 then 0xA90 and offset by 4 to find exact
     //gold value
-    ReadProcessMemory(wesnoth_process, (void*)0x017EED18, &gold_value, 4, &bytes_read);
+    int rd_pNo = ReadProcessMemory(wesnoth_process, (void*)0x017EED18, &gold_value, 4, &bytes_read);
+    readprocess_errorMsg();
 
     gold_value += 0xA90;
 
-    ReadProcessMemory(wesnoth_process, (void*)gold_value, &gold_value, 4, &bytes_read);
+    rd_pNo = ReadProcessMemory(wesnoth_process, (void*)gold_value, &gold_value, 4, &bytes_read);
+    if (rd_pNo = 0)
+        readprocess_errorMsg();
 
     gold_value += 4;
 
-    WriteProcessMemory(wesnoth_process, (void*)gold_value, &new_gold_amount, 4, &bytes_written);
+    rd_pNo = ReadProcessMemory(wesnoth_process, (void*)gold_value, &gold_value, 4, &bytes_read);
+    if (rd_pNo = 0)
+        readprocess_errorMsg();
+    cout << "\n\nInitial Gold Value is: " << gold_value;
 
-    ReadProcessMemory(wesnoth_process, (void*)gold_value, &gold_value, 4, &bytes_read);
+    int wrt = WriteProcessMemory(wesnoth_process, (void*)gold_value, &new_gold_value, 4, &bytes_written);
+    if (wrt == 0)
+    {
+          cout << "\nWrite Process Failed!\n";
+          system("pause");
+          exit(99);
+    }
 
-    cout<< "Gold value: "<< gold_value<< "\nWesnoth process: " << wesnoth_process << "\nNew gold amount: "<< new_gold_amount <<endl;
+    int rd_pNo1 = ReadProcessMemory(wesnoth_process, (void*)gold_value, &gld_val, 4, &bytes_read);
+    if (rd_pNo1 = 0)
+        readprocess_errorMsg();
+
+    cout<< "\nNew Gold Value is: "<< gld_val;
+
     return (0);
+}
+
+void readprocess_errorMsg(void)
+{
+        cout << "\n\nERROR: Read Process Failed!\n\n";
+        system("pause");
+        exit(98);
 }
